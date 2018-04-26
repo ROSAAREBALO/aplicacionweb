286d54de50cc1d5cf5df8e2b1e0325446cfec3f4
ROSAAREBALO <rousetemari@gmail.com>
2018-04-26 11:29:11 -0400

    iniciando el proyecto

---
+++ src/main/java/proyectogrado/aplicacionweb/AplicacionwebApplication.java
@@ -0,0 +1,21 @@
+package proyectogrado.aplicacionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class AplicacionwebApplication {
+	@RequestMapping("/")
+	String home(){
+		return "hola mundo";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionwebApplication.class, args);
+	}
+}
